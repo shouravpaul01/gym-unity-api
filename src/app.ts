4ec55d26539527f8553d26b1e7b0400ac1c158ba
Shourav Paul <paul.shourav01@gmail.com>
2025-08-17 23:47:53 +0600

    Intial setup

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("API is running 🚀");
+});
+
+export default app;
